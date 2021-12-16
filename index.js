31d5503c299739663aa88a39ef6925e780a76186
Haleem Hamid <hhamidbiz@gmail.com>
2021-12-16 10:38:36 -0800

    commit tree 536515e35b33ae5368b56b8e88a2cd220300eb9f

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(array, name){
+    return array.filter(item => item.toUpperCase() === name.toUpperCase())
+}
+
+function fuzzyMatch(array, name){
+    return array.filter(item => item[0]=== name[0])
+}
+
+function matchName(obj, name){
+    return obj.filter(item => item.name === name )
+}
